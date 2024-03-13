04d13967a239fe76e7a9ec9e798a36815ba55ac8
Yasir-Alii <binjabbar10@gmail.com>
2024-03-13 06:07:19 -0700

    Added new file

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("CONNECTED");
+let input = prompt("ENTER A NUMBER FOR TABLE ");
+let n = 10;
+for (let index = 1; index <= n; index++) {
+  console.log(input, " X ", index, " = ", input * index);
+}
